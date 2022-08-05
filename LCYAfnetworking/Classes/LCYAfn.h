556d13e3cbcea37c90f91a164118de5adedbb25c
李存瑶 <cyli@streamax.com>
2022-08-05 16:23:14 +0800

    组件化demo

---
+++ LCYAfnetworking/Classes/LCYAfn.h
@@ -0,0 +1,16 @@
+//
+//  LCYAfn.h
+//  LCYAfnetworking
+//
+//  Created by lcy on 2022/8/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCYAfn : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LCYAfnetworking/Classes/LCYAfn.m
@@ -0,0 +1,12 @@
+//
+//  LCYAfn.m
+//  LCYAfnetworking
+//
+//  Created by lcy on 2022/8/5.
+//
+
+#import "LCYAfn.h"
+
+@implementation LCYAfn
+
+@end
