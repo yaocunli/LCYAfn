8b3a4e03cc7298f2620e5ef6f4f047fc1b3b7588
李存瑶 <cyli@streamax.com>
2022-08-05 16:42:00 +0800

    组件化demo

--- LCYAfnetworking/Classes/LCYAfn.m
+++ LCYAfnetworking/Classes/LCYAfn.m
@@ -6,7 +6,18 @@
 //
 
 #import "LCYAfn.h"
+#import "AFNetworking.h"
 
 @implementation LCYAfn
-
++(void)testWithString:(NSString*)url {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager GET:url parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSString *dataString = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",dataString);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+    }];
+}
 @end
